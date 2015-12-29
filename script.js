05a4bbd485500d1e4f8b0143efde8cf5d2e6553d
Guilherme Cavichioli <guilhermecavichioli@hotmail.com>
2015-12-29 02:53:13 -0200

    Fixed somethings

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 
-$(".board").mouseover(function(){
-alert("O cursor foi posicionado sobre a div.");
-});
+  /**
+   * Cuidado, sua div.board não contém nada, portanto ela pode ser ignorada por alguns navegadores
+   * ou seja, o mouseover não acontecerá!
+   */
+  $(".board").mouseover(function(){
+    alert("O cursor foi posicionado sobre a div.");
+  });
 
 });
